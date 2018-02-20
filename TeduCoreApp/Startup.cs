7fa8e298cc53c2568b83adf838c0a05fccbf49c9
QuangTung VoLe <volequangtung@gmail.com>
2018-02-20 16:39:36 +0700

    commit cho lession7

--- TeduCoreApp/Startup.cs
+++ TeduCoreApp/Startup.cs
@@ -24,6 +24,9 @@ namespace TeduCoreApp
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // Add some comment
+        //
+        //
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
